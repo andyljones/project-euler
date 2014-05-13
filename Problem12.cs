3aa74251d1a9cdeb38e0159236e03b8f27af24db
Andy L Jones <andyjones.ed@gmail.com>
2014-05-13 11:12:55 +0100

    Problems 12, 13, 14

---
+++ Problem12.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Problem12 
+{
+  private const int MIN_DIVISORS = 500;
+
+  public static void Main() 
+  {
+    int n = 1;
+    while (NumberOfDivisors(TriangularNumber(n)) < MIN_DIVISORS) 
+    {
+      n = n + 1; 
+    }
+    
+    Console.WriteLine("First triangular number with more than " + MIN_DIVISORS + " divisors is " + TriangularNumber(n));
+  }
+  
+  private static int TriangularNumber(int n)
+  {
+    return n*(n+1)/2;
+  }
+
+  private static int NumberOfDivisors(int n) 
+  {
+    int count = 0;
+    for (int i = 1; i < Math.Sqrt(n); i++) {
+      bool iIsDivisorOfn = (n/i*i == n);
+
+      if (iIsDivisorOfn) 
+      { 
+        count = count + 2;
+      }
+    }
+
+    if (Math.Sqrt(n) * Math.Sqrt(n) == n) 
+    {
+      count = count + 1;
+    }
+   
+    return count;
+  }
+}
